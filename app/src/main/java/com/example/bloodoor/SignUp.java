f5fef677c6b1b03f87e5f6cd7f308d2f7e306b5f
pranav309 <pranavpawar309@gmail.com>
2021-09-24 19:46:55 -0700

    SignUp activity created...

---
+++ app/src/main/java/com/example/bloodoor/SignUp.java
@@ -0,0 +1,22 @@
+package com.example.bloodoor;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
+
