8d6188b079561d5dc8133047d17d03a5ded32b88
pranav309 <pranavpawar309@gmail.com>
2021-11-03 13:35:48 -0700

    NAvigation menu bar added...

--- app/src/main/java/com/example/bloodoor/Homepage_user.java
+++ app/src/main/java/com/example/bloodoor/Homepage_user.java
@@ -1,28 +1,127 @@
 package com.example.bloodoor;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
 
 import io.alterac.blurkit.BlurLayout;
 
-public class Homepage_user extends AppCompatActivity {
+public class Homepage_user extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     BlurLayout blurLayout1;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);     //removes title bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        Objects.requireNonNull(getSupportActionBar()).hide();    //removes action bar
-        blurLayout1 = findViewById(R.id.blurLayout);
+        Objects.requireNonNull(getSupportActionBar()).show();    //removes action bar
+        blurLayout1 = findViewById(R.id.blurLayout1);
         setContentView(R.layout.activity_homepage_user);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.home);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.home:
+                break;
+            case R.id.profile:
+                Intent intent = new Intent(Homepage_user.this, profileUser.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
+            case R.id.about:
+                Intent intent1 = new Intent(Homepage_user.this, AboutUS.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent1);
+                break;
+            case R.id.faq:
+                Intent intent2 = new Intent(Homepage_user.this, FAQpage.class);
+                intent2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent2);
+                break;
+            case R.id.logOut:
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Homepage_user.this);
+                alertDialogBuilder.setTitle("Confirm Exit..!!");
+                alertDialogBuilder.setIcon(R.drawable.ic_exit);
+                alertDialogBuilder.setMessage("Are you sure ?");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(getApplicationContext(),SignUp_User.class);
+                        mAuth.signOut();
+                        startActivity(intent);
+                        Toast.makeText(Homepage_user.this,"Logged out successfully",Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
+                alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Homepage_user.this,"Cancelled..",Toast.LENGTH_LONG).show();
+                    }
+                });
+                alertDialogBuilder.show();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
