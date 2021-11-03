02564ae0cacf9e6294a6e5e29a416229b92e22f1
onkarnp <opanchare123@gmail.com>
2021-11-03 16:24:37 +0530

    template activity

--- app/src/main/java/com/example/bloodoor/template.java
+++ app/src/main/java/com/example/bloodoor/template.java
@@ -2,24 +2,14 @@ package com.example.bloodoor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.icu.text.MessagePattern;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.os.Handler;
-import com.airbnb.lottie.LottieAnimationView;
-
-import java.util.Objects;
 
 import io.alterac.blurkit.BlurLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class template extends AppCompatActivity {
 
-    private static  int SPLASH_SCREEN = 3000;
     BlurLayout blurLayout;
 
     @Override
@@ -28,20 +18,7 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);     //removes title bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         blurLayout = findViewById(R.id.blurLayout);         //for blurring background
-        setContentView(R.layout.activity_main);
-
-        ImageView logo = findViewById(R.id.logo);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
-        logo.setAnimation(animation);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this,options.class);
-                startActivity(intent);
-                finish();
-            }
-        },SPLASH_SCREEN);
+        setContentView(R.layout.activity_template);
     }
 
     //Functions for making background blurr
@@ -58,5 +35,4 @@ public class MainActivity extends AppCompatActivity {
         blurLayout.pauseBlur();
         super.onStop();
     }
-
 }
