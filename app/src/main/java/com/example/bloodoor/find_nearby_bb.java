c2abffa2659f9117cbd658fcd1b8e57d1724cd8d
pranav309 <pranavpawar309@gmail.com>
2021-11-13 01:22:38 -0800

    Find Blood Banks done...

--- app/src/main/java/com/example/bloodoor/find_nearby_bb.java
+++ app/src/main/java/com/example/bloodoor/find_nearby_bb.java
@@ -1,24 +1,134 @@
 package com.example.bloodoor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 
 import io.alterac.blurkit.BlurLayout;
+
+import static com.example.bloodoor.R.string.google_map_key;
 
 public class find_nearby_bb extends AppCompatActivity {
 
     BlurLayout blurLayout;
+    Spinner spType;
+    Button btFind;
+    SupportMapFragment supportMapFragment;
+    GoogleMap map;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    double currentLat = 0, currentLong = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);     //removes title bar
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         blurLayout = findViewById(R.id.blurLayout);         //for blurring background
         setContentView(R.layout.activity_find_nearby_bb);
+
+        ///Assign variable
+        spType = findViewById(R.id.sp_type);
+        btFind = findViewById(R.id.bt_find);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+
+        //Initialize array of place type
+        String[] placeTypeList = {"hospitals", "bloodbanks", "atm"};
+
+        //Initialize array of place name
+        String[] placeNameList = {"Hospitals", "Blood Banks", "ATM"};
+
+        //Set adapter on spinner
+        spType.setAdapter(new ArrayAdapter<>(find_nearby_bb.this, android.R.layout.simple_spinner_dropdown_item, placeNameList));
+
+        //Initialize fused location provider client
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(find_nearby_bb.this);
+
+        //Check permission
+        if (ActivityCompat.checkSelfPermission(find_nearby_bb.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Task<Location> task = fusedLocationProviderClient.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        currentLat = location.getLatitude();
+                        currentLong = location.getLongitude();
+                        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                            @Override
+                            public void onMapReady(@NonNull GoogleMap googleMap) {
+                                map = googleMap;
+                                map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(currentLat, currentLong), 10
+                                ));
+                            }
+                        });
+                    }
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(find_nearby_bb.this
+                    , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+
+        btFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i = spType.getSelectedItemPosition();
+                String url = "https://maps.googleapis.com/maps/api/place/nearbyserch/json"
+                        + "?location=" + currentLat + "," + currentLong
+                        + "&radius=10000"
+                        + "&types=" + placeTypeList[i]
+                        + "&sensor=true"
+                        + "&key=" + getResources().getString(google_map_key);
+            }
+        });
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentFocus();
+            }
+        }
     }
 
     //Functions for making background blurr
@@ -34,5 +144,80 @@ public class find_nearby_bb extends AppCompatActivity {
     protected void onStop() {
         blurLayout.pauseBlur();
         super.onStop();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class PlaceTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String data = null;
+            try {
+                data = downloadUrl(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            new ParserTask().execute(s);
+
+        }
+    }
+
+    private String downloadUrl(String string) throws IOException {
+        URL url = new URL(string);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+
+        InputStream stream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+        String line = "";
+
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+        }
+
+        String data = builder.toString();
+        reader.close();
+        return data;
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class ParserTask extends AsyncTask<String,Integer, List<HashMap<String,String>>> {
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... strings) {
+            JsonParser jsonParser = new JsonParser();
+            List<HashMap<String, String>> mapList = null;
+            JSONObject object = null;
+            try {
+                object = new JSONObject(strings[0]);
+                mapList = jsonParser.parseResult(object);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return mapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+            map.clear();
+            for(int i=0; i<hashMaps.size(); i++){
+                HashMap<String, String> hashMapList = hashMaps.get(i);
+                double lat = Double.parseDouble(hashMapList.get("lat"));
+                double lng = Double.parseDouble(hashMapList.get("lng"));
+                String name =  hashMapList.get("name");
+
+                LatLng latLng = new LatLng(lat, lng);
+                MarkerOptions options =  new MarkerOptions();
+                options.position(latLng);
+                options.title(name);
+                map.addMarker(options);
+            }
+
+        }
     }
 }
